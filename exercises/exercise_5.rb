0fe6fd6d1d0fd00e6e296beff5e95dd58dea0556
Harsh Patel <hpatel1959@hotmail.com>
2023-04-06 09:24:16 +0000

    finish exercises

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: $#{total_revenue}"
+
+avg_total_revenue = Store.average(:annual_revenue)
+puts "Total Average Revenue: $#{avg_total_revenue}"
+
+stores_with_million_ar = Store.where("annual_revenue >= ?", 1000000).count
+puts stores_with_million_ar
+
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Harsh", last_name: "Patel", hourly_rate: 50)
+@store1.employees.create(first_name: "Truman", last_name: "Chan", hourly_rate: 40)
+@store1.employees.create(first_name: "Niral", last_name: "Mistry", hourly_rate: 30)
+@store1.employees.create(first_name: "Ricky", last_name: "Wang", hourly_rate: 20)
+
+@store2.employees.create(first_name: "Adrian", last_name: "Sammy", hourly_rate: 55)
+@store2.employees.create(first_name: "Michael", last_name: "Chang", hourly_rate: 45)
+@store2.employees.create(first_name: "Kevin", last_name: "Lee", hourly_rate: 35)
+@store2.employees.create(first_name: "Vivek", last_name: "Chand", hourly_rate: 25)
+@store2.employees.create(first_name: "Jason", last_name: "Cao", hourly_rate: 15)
